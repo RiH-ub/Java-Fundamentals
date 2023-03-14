f7d20a16a7f2f6ce7ff78dbc5dce9de0f7b3a40c
RiH-ub <renzozahir2001@gmail.com>
2023-03-14 10:42:01 -0500

    hola

--- Fundamentos.java
+++ Fundamentos.java
@@ -6,14 +6,24 @@ public class Fundamentos {
         int sueldo = 3000; // Entera
         double pi = 3.1416; // Decimales (float)
         boolean estado = true; // Booleano
+        double promedio = (15 + 12 + 15) / 3;
+        Double centigrados, farhenhei = 98D;
 
 
+    // float pi = 3.1416f; // Decimales (float)
         System.out.println(nombre);
         System.out.println(sueldo);
         System.out.println(pi);
         System.out.println(estado);
-            
-        if (estado)
+        
+        System.out.println(promedio);
+
+    centigrados = ((farhenhei - 32) * 5) / 9;
+    System.out.println(centigrados);
+    int temperatura = centigrados.intValue();
+    System.out.println(temperatura);
+
+        
 
 
 
