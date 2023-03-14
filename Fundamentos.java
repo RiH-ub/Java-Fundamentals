14ebcac4984e6293d796d4c6d509af231b128a8f
RiH-ub <renzozahir2001@gmail.com>
2023-03-14 09:57:47 -0500

    nombre

---
+++ Fundamentos.java
@@ -0,0 +1,36 @@
+public class Fundamentos {
+
+
+    public static void main(String[] args) {
+        String nombre = "Renzo"; // Cadena (Texto)
+        int sueldo = 3000; // Entera
+        double pi = 3.1416; // Decimales (float)
+        boolean estado = true; // Booleano
+
+
+        System.out.println(nombre);
+        System.out.println(sueldo);
+        System.out.println(pi);
+        System.out.println(estado);
+            
+        if (estado)
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+ 
+
+
+
+
+
+}
