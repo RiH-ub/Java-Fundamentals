db82eed7ea9402033332a97a95d108409bab536f
RiH-ub <renzozahir2001@gmail.com>
2023-03-29 01:49:56 +0000

    j

--- Bidimensional01.java
+++ Bidimensional01.java
@@ -5,10 +5,19 @@ public class Bidimensional01 {
         
         for (int i = 0; i < miArray.length; i++) {      // arreglo con Arraylist         
             for (int j = 0; j < miArray[0].length; i++) {
-
+                miArray[1] [3] = i + j;
             }
             System.out.println(i);
         }
 
+    }
+
+    int[] arr = new int[4];
+    arr[0] = 1;
+    arr[1] = 5;
+    arr[2] = 10;
+    arr[3] = 15;
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(Arrays.miArray[i]));
     }
 }
