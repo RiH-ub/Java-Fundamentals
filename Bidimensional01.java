43e0238e7300007573010e1158d1f800f22b0412
RiH-ub <renzozahir2001@gmail.com>
2023-03-29 00:59:27 +0000

    hola

---
+++ Bidimensional01.java
@@ -0,0 +1,12 @@
+public class Bidimensional01 {
+
+    public static void main(String[] args) {
+        int[] [] miArray = new int[3] [4];
+        
+        for (int i = 0; i < miArray.length; i++) {      // arreglo con Arraylist         
+            for (int i = )
+            System.out.print
+        }
+
+    }
+}
