a0cba3011dac3b940363899d6b426a71f8a63ad3
RiH-ub <renzozahir2001@gmail.com>
2023-03-29 03:25:50 +0000

    hola

--- Matriz.java
+++ Matriz.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Matriz {
 
     public static void main(String[] args) {
-        int[][] miArray = new int[4][4];
+        int[][] miArray = new int[3][3];
         Scanner scanner = new Scanner(System.in);
 
         // Llenar la matriz con valores introducidos por el usuario
@@ -21,28 +21,22 @@ public class Matriz {
             System.out.println(Arrays.toString(miArray[i]));
         }
 
-        // Imprimir la diagonal de la matriz
-        System.out.println("\nDiagonal:");
-        for (int i = 0; i < miArray.length; i++) {
-            System.out.print(miArray[i][i] + " ");
-        }
-
         // Encontrar el número mayor y menor de la matriz
-        int mayor = miArray[0][0];
-        int menor = miArray[0][0];
+        int maximo = miArray[0][0];
+        int minimo = miArray[0][0];
         for (int i = 0; i < miArray.length; i++) {
             for (int j = 0; j < miArray[0].length; j++) {
-                if (miArray[i][j] > mayor) {
-                    mayor = miArray[i][j];
+                if (miArray[i][j] > maximo) {
+                    maximo = miArray[i][j];
                 }
-                if (miArray[i][j] < menor) {
-                    menor = miArray[i][j];
+                if (miArray[i][j] < minimo) {
+                    minimo = miArray[i][j];
                 }
             }
         }
 
         // Imprimir el número mayor y menor de la matriz
-        System.out.println("\nEl número mayor de la matriz es: " + mayor);
-        System.out.println("El número menor de la matriz es: " + menor);
+        System.out.println("El número mayor de la matriz es: " + maximo);
+        System.out.println("El número menor de la matriz es: " + minimo);
     }
 }
