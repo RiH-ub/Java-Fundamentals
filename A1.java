7232afa751e96d71b708e53f9d1eb0d20073eacc
The Renzo Code <renzozahir2001@gmail.com>
2023-11-02 22:15:06 -0500

    ho

--- A1.java
+++ A1.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
-public class A1{
+public class A1 {
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa un numero: ");
+
+        System.out.print("Dime tu nombre: ");
+        String datos = sc.nextLine();
+        System.out.println("Tu nombre es: " + datos);
+
+        System.out.print("Dime tu edad: ");
+        int edad = sc.nextInt();
+        System.out.println("Tienes " + edad + " años.");
     }
 }
