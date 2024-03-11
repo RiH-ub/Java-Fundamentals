40677eca36aa8a442266b68391a8ee59dd6b8b27
The Renzo Code <renzozahir2001@gmail.com>
2024-03-11 12:26:31 -0500

    555

--- A.java
+++ A.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class A {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     }
 }
