c67bbec5811b5a3e80b1ee372527c2d339b9f7d0
The Renzo Code <renzozahir2001@gmail.com>
2024-10-01 09:14:01 -0500

    Practica

--- A.java
+++ A.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class A{
-    public static void main(String args[]){
+    public static void main(String Args[]){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Como te llamas: ");
-        String nom = sc.nextLine();
+        System.out.print("Dime tu nombre ");
+        String nombre = sc.nextLine();
 
+        System.out.println("¿Te llamas: " + nombre + "?, Que buen nombre!");
+
+        System.out.print("Dime tu edad: ");
+        int edad = sc.nextInt();
+
+        System.out.println("Tu tienes: "+ edad + " años" );
     }
 }
