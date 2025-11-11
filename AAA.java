51b4a9dd70121b7f979b6eb38c3ebab8f4e3b7b8
RiH-ub <renzozahir2001@gmail.com>
2025-11-11 00:01:53 -0500

    hola

--- AAA.java
+++ AAA.java
@@ -1,24 +1,11 @@
 import java.util.Scanner;
 
-public class A{
+public class AAA{
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hola");
 
-        System.out.print("Dime tu nombre: ");
         String nombre = sc.nextLine();
-
-        System.out.println("Tu nombre es " + nombre);
-
-        System.out.print("Dime el primer numero:  ");
-        int suma1 = sc.nextInt();
-
-        System.out.print("Dime el segundo numero: ");
-        int suma2 = sc.nextInt();
-
-        int respuesta = suma1 + suma2;
-
-        System.out.print("Tu respuesta es: " + respuesta );
-
 
     }
 }
