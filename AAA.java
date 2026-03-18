b89d080056d39fe1649186ed42c0a04d78dbd957
RiH-ub <renzozahir2001@gmail.com>
2026-03-17 19:45:07 -0500

    v ccg

---
+++ AAA.java
@@ -0,0 +1,13 @@
+immport java.util.Scanner*;
+
+public class AAA {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, este es el archivo AAA.java");
+
+
+        
+    
+    
+    }
+}   
